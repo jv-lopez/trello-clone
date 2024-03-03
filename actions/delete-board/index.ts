e8ffc9a02c8f664ad52247f070d31d47d9edc5db
me-jameshwartlopez <me@jameshwartlopez.com>
2024-03-03 16:06:45 +0800

    feat: board navbar and actions to delete and update board title

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -1,14 +1,52 @@
 'use server';
 
-import { db } from '@/lib/db';
+import { auth } from '@clerk/nextjs';
 import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
 
-export const deleteBoard = async (id: string) => {
-  await db.board.delete({
-    where: {
-      id: id,
-    },
-  });
+import { db } from '@/lib/db';
+import { createSafeAction } from '@/lib/create-safe-action';
 
-  revalidatePath('/organization/org_2cHviyphJkWRYXjsrVW0Ml5nGbE');
+import { DeleteBoard } from './schema';
+import { InputType, ReturnType } from './types';
+// import { createAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from '@prisma/client';
+import { decreaseAvailableCount } from '@/lib/org-limit';
+import { checkSubscription } from '@/lib/subscription';
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const { userId, orgId } = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: 'Unauthorized',
+    };
+  }
+
+  const isPro = await checkSubscription();
+
+  const { id } = data;
+  let board;
+
+  try {
+    board = await db.board.delete({
+      where: {
+        id,
+        orgId,
+      },
+    });
+
+    if (!isPro) {
+      await decreaseAvailableCount();
+    }
+  } catch (error) {
+    return {
+      error: 'Failed to delete.',
+    };
+  }
+
+  revalidatePath(`/organization/${orgId}`);
+  redirect(`/organization/${orgId}`);
 };
+
+export const deleteBoard = createSafeAction(DeleteBoard, handler);
