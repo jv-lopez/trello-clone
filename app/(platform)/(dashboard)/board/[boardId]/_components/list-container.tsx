0211dd177c9faffd4c64c6c783c4cab7e096f641
me-jameshwartlopez <me@jameshwartlopez.com>
2024-03-04 20:05:42 +0800

    feat: update items list and copy items list

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -1,8 +1,16 @@
-import { ListWithCards } from '@/types';
+"use client";
 
+import { toast } from "sonner";
+import { useEffect, useState } from "react";
+// import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+
+import { ListWithCards } from "@/types";
+import { useAction } from "@/hooks/use-action";
+// import { updateListOrder } from "@/actions/update-list-order";
+// import { updateCardOrder } from "@/actions/update-card-order";
 
 import { ListForm } from "./list-form";
-// import { ListItem } from "./list-item";
+import { ListItem } from "./list-item";
 
 type ListContainerProps = {
   data: ListWithCards[];
@@ -13,8 +21,23 @@ export const ListContainer = ({
   data,
   boardId
 }: ListContainerProps) => {
+  const [orderedData, setOrderedData] = useState<ListWithCards[]>();
+
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
   return (
-    <ol>
+    <ol className="flex gap-x-3 h-full">
+      {orderedData && orderedData.map((list, index) => {
+        return (
+          <ListItem
+            key={list.id}
+            index={index}
+            data={list}
+          />
+        )
+      })}
       <ListForm />
       <div className='flex-shrink-0 w-1' />
     </ol>
