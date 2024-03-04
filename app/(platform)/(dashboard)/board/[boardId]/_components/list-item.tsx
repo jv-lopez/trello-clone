e7e591b360fe8cd2e3fc9364b203e398c6c721c1
me-jameshwartlopez <me@jameshwartlopez.com>
2024-03-04 20:57:51 +0800

    feat: show card items list

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-item.tsx
@@ -7,7 +7,7 @@ import { cn } from "@/lib/utils";
 import { ListWithCards } from "@/types";
 
 import { CardForm } from "./card-form";
-// import { CardItem } from "./card-item";
+import { CardItem } from "./card-item";
 import { ListHeader } from "./list-header";
 
 type ListItemProps = {
@@ -42,6 +42,20 @@ export const ListItem = ({
           onAddCard={enableEditing}
           data={data}
         />
+        <ol
+          className={cn(
+            "mx-1 px-1 py-0.5 flex flex-col gap-y-2",
+            data.cards.length > 0 ? "mt-2" : "mt-0",
+          )}
+        >
+          {data.cards.map((card, index) => (
+            <CardItem
+              index={index}
+              key={card.id}
+              data={card}
+            />
+          ))}
+        </ol>
         <CardForm
           listId={data.id}
           ref={textareaRef}
